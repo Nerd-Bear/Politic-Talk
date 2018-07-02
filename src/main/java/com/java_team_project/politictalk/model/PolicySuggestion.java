0b42db92584e7b9e3579db06e4288b656d9bc765
RISMME <python@istruly.sexy>
2018-07-03 08:45:48 +0900

    UPDATE: Database Architecture

---
+++ src/main/java/com/java_team_project/politictalk/model/PolicySuggestion.java
@@ -0,0 +1,13 @@
+package com.java_team_project.politictalk.model;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+public class PolicySuggestion {
+    @Id
+    private String suggestPolicyId;
+    private String author;
+    private Date date;
+    private 
+}
