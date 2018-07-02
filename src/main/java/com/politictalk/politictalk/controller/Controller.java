cf4ae55aea23367aa6a52e694dd1cf53359bc134
RISMME <python@istruly.sexy>
2018-07-02 11:22:36 +0900

    UPDATE: Swagger

--- src/main/java/com/politictalk/politictalk/controller/Controller.java
+++ src/main/java/com/politictalk/politictalk/controller/Controller.java
@@ -1,5 +1,6 @@
-package com.politictalk.politictalk.web;
+package com.politictalk.politictalk.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WebRestController{
+public class Controller {
+    @ApiOperation(value = "isServerRunning", notes = "서버가 실행되고 있는지 확인하는 API", tags = "Test")
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public String isServerRunning(){
