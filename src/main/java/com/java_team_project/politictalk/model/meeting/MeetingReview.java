328ed07d251e336f9e97105480fc7c45f5f66812
RISMME <python@istruly.sexy>
2018-07-23 21:53:40 +0900

    UPDATE: meeting repository

--- src/main/java/com/java_team_project/politictalk/model/meeting/MeetingReview.java
+++ src/main/java/com/java_team_project/politictalk/model/meeting/MeetingReview.java
@@ -4,22 +4,18 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.ArrayList;
 import java.util.Date;
 
 @Data
-@Document(collection = "meeting")
-public class Meeting {
+@Document(collection = "meeting_review")
+public class MeetingReview {
     @Id
-    private String meetingId;
+    private String meetingReviewId;
     private String author;
     private Date date;
     private String title;
     private String content;
     private int viewCount;
+    private String meetingId;
     private String politicianId;
-    private Date meetingDate;
-    private ArrayList<String> participantId;
-    private boolean didPolicianRead;
-    private String region;
 }
