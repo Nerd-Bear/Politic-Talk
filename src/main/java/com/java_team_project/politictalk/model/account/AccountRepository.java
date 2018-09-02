07b811f43eb590b8f56b5ce31ca5c2ae7c8d656c
RISMME <python@istruly.sexy>
2018-09-02 20:02:53 +0900

    Delete: model repositories

--- src/main/java/com/java_team_project/politictalk/model/account/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.java_team_project.politictalk.model.account;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AccountRepository extends MongoRepository<Account, Long>, AccountRepositoryCustom {
-    Account findFirstById(String id);
-
-}
