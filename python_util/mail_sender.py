e9bc5fc3c13195427b4d539436cbd20ef9717c16
RISMME <python@istruly.sexy>
2018-09-03 08:36:47 +0900

    Update: environment variable naming convention

--- python_util/mail_sender.py
+++ python_util/mail_sender.py
@@ -4,28 +4,28 @@ import smtplib
 import argparse
 from email.mime.text import MIMEText
 
-email_address = os.getenv('politictalk_email_address')
-email_password = os.getenv('politictalk_email_password')
+EMAIL_ADDRESS = os.getenv('POLITICTALK_EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.getenv('POLITICTALK_EMAIL_PASSWORD')
 
 smtp = smtplib.SMTP('smtp.gmail.com', 587)
 smtp.starttls()
-smtp.login('email_address', 'email_password')
+smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
 
-def send(reciever, subject='test', message='test'):
+def send(receiver, subject='test', message='test'):
     msg = MIMEText(message)
     msg['Subject'] = subject
-    msg['To'] = reciever
-    msg['From'] = email_address
-    smtp.sendmail(email_address, reciever, msg.as_string())
+    msg['To'] = receiver
+    msg['From'] = EMAIL_ADDRESS
+    smtp.sendmail(EMAIL_ADDRESS, receiver, msg.as_string())
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('reciever')
+    parser.add_argument('receiver')
     parser.add_argument('subject')
     parser.add_argument('message')
 
     args = parser.parse_args()
 
-    send(args.reciever, args.subject, args.message)
+    send(args.receiver, args.subject, args.message)
