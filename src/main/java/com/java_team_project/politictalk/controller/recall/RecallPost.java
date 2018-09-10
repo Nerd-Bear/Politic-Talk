cf09c982337b2d52df5ea2473614b2adabceef39
RISMME <python@istruly.sexy>
2018-09-10 22:26:35 +0900

    Update: get recall

--- src/main/java/com/java_team_project/politictalk/controller/recall/RecallPost.java
+++ src/main/java/com/java_team_project/politictalk/controller/recall/RecallPost.java
@@ -1,5 +1,6 @@
 package com.java_team_project.politictalk.controller.recall;
 
+import com.java_team_project.politictalk.exception.NoContentException;
 import com.java_team_project.politictalk.model.recall.Recall;
 import com.java_team_project.politictalk.model.recall.RecallRepository;
 import io.swagger.annotations.Api;
@@ -32,15 +33,17 @@ public class RecallPost {
         return recall.getRecallId();
     }
 
-    @ApiOperation(value = "Get Discontent", notes = "Get Discontent")
-    @RequestMapping(value = "/recall/discontent", method = RequestMethod.GET)
+    @ApiOperation(value = "Get Recall", notes = "Get Recall")
+    @RequestMapping(value = "/recall", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public HashMap<String, Object> getDiscontent(@RequestParam UUID discontentId) {
-        /*
-        불만 게시글 id를 받아 DB에서 찾아서 게시글에 대한 내용을 리턴함
-         */
-        HashMap<String, Object> discontent = new HashMap<>();
-        return discontent;
+    public Recall getRecall(@RequestParam String recallId) {
+
+        Recall recall = recallRepository.findByRecallId(recallId);
+        if(recall == null){
+            throw new NoContentException();
+        }
+
+        return recall;
     }
 
     @ApiOperation(value = "Get Discontent List", notes = "Get Discontent List")
@@ -63,5 +66,11 @@ public class RecallPost {
          */
         HashMap<String, Object> discontentList = new HashMap<>();
         return discontentList;
+    }
+
+    @ExceptionHandler(NoContentException.class)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void noContentException(){
+
     }
 }
--- src/main/java/com/java_team_project/politictalk/model/recall/RecallRepository.java
+++ src/main/java/com/java_team_project/politictalk/model/recall/RecallRepository.java
@@ -3,4 +3,5 @@ package com.java_team_project.politictalk.model.recall;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface RecallRepository extends MongoRepository<Recall, String> {
+    Recall findByRecallId(String recallId);
 }
